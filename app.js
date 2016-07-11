cd02efb19541e38a4cc13ac45aa04d1a9fe3758b
Elad Katz <eladkt@gmail.com>
2016-07-11 22:33:16 +0300

    cw-1

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // this is the main file.
 var express = require('express');
-
-var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.use(express.static('public'));
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
